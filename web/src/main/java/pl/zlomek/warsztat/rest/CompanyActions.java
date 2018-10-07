8c173fe3a33fbbcf2f800803b98794ac5ff9c453
Krzysztof Drzazga <drzazga_krzysztof@o2.pl>
2018-10-07 10:45:39 +0200

    Edit company module created

--- web/src/main/java/pl/zlomek/warsztat/rest/CompanyActions.java
+++ web/src/main/java/pl/zlomek/warsztat/rest/CompanyActions.java
@@ -5,9 +5,11 @@ import org.slf4j.LoggerFactory;
 import pl.zlomek.warsztat.data.CarServiceDataRespository;
 import pl.zlomek.warsztat.data.CompaniesRepository;
 import pl.zlomek.warsztat.data.CompanyDataRespository;
+import pl.zlomek.warsztat.data.EmployeesRepository;
 import pl.zlomek.warsztat.model.*;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
@@ -23,15 +25,18 @@ public class CompanyActions {
     @Inject
     CarServiceDataRespository carServiceDataRespository;
 
+    @Inject
+    EmployeesRepository employeesRepository;
+
     Logger log = LoggerFactory.getLogger(CompanyActions.class);
 
     @POST
     @Path("/addCompany")
+    @Transactional
     public Response addCompany(AddCompanyForm form){
         Company company = companiesRepository.getCompanyByName(form.getName());
         if(company != null){
-            log.info("nie istnieje");
-            return Response.status(400).entity(new ErrorResponse("Podana funkcja istnieje")).build();
+            return Response.status(400).entity(new ErrorResponse("Podana firma istnieje")).build();
         }
         String companyName = form.getName();
         String nip = form.getNip();
@@ -48,6 +53,7 @@ public class CompanyActions {
 
     @POST
     @Path("/addCompanyData")
+    @Transactional
     public Response addCompanyData(AddCompanyDataForm form){
         CompanyData companyData =  companyDataRespository.getCompanyDataByName(form.getName());
         if(companyData != null){
@@ -67,6 +73,7 @@ public class CompanyActions {
 
     @POST
     @Path("/addCarServiceData")
+    @Transactional
     public Response addCarServiceData(AddCarServiceDataForm form){
         String serviceName = form.getServiceName();
         String nip = form.getNip();
@@ -79,5 +86,40 @@ public class CompanyActions {
         CarServiceData carServiceData = new CarServiceData(nip, email, serviceName, cityName, streetName, buildNum, aptNum, zipCode);
         carServiceDataRespository.insert(carServiceData);
         return Response.status(200).build();
+    }
+
+    @POST
+    @Path("/editCompany")
+    @Transactional
+    public Response editCompany(EditCompanyForm form){
+        Employee employee = employeesRepository.findByToken(form.getAccessToken());
+        if(employee == null){
+            return Response.status(401).build();
+        }
+        String accessToken = employeesRepository.generateToken(employee);
+        Company company = companiesRepository.getCompanyByName(form.getCurrentName());
+        if (company == null){
+            return Response.status(400).entity(accessToken).build();
+        }
+        setCompanyFields(company,form);
+        companiesRepository.updateCompany(company);
+        return Response.status(200).entity(accessToken).build();
+    }
+
+    public void setCompanyFields(Company company, EditCompanyForm form){
+        if(form.getAptNum()!= null)
+            company.setAptNum(form.getAptNum());
+        if(form.getBuildingNum()!= null)
+            company.setBuildingNum(form.getBuildingNum());
+        if(form.getCityName()!= null)
+            company.setCityName(form.getCityName());
+        if(form.getEmail()!= null)
+            company.setEmail(form.getEmail());
+        if(form.getName()!= null)
+            company.setCompanyName(form.getName());
+        if(form.getZipCode() != null)
+            company.setZipCode(form.getZipCode());
+        if(form.getStreetName()!= null)
+            company.setStreetName(form.getStreetName());
     }
 }
