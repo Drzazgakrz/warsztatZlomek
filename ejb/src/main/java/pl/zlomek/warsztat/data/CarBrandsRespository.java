fae38f3a49ee8227e7019787afeb4ff5ee889def
Dominik <dominikfijalek@gmail.com>
2018-10-07 17:06:53 +0200

    added car brand insert into DB

---
+++ ejb/src/main/java/pl/zlomek/warsztat/data/CarBrandsRespository.java
@@ -0,0 +1,32 @@
+package pl.zlomek.warsztat.data;
+
+import pl.zlomek.warsztat.model.CarBrand;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+
+@ApplicationScoped
+public class CarBrandsRespository {
+
+    @Inject
+    private EntityManager em;
+
+    @Transactional
+    public void saveCarBrand(CarBrand carBrand){
+        em.persist(carBrand);
+    }
+
+    public CarBrand getCarBrandByName(String name){
+        try {
+            TypedQuery<CarBrand> query = em.createQuery("SELECT carBrand FROM CarBrand carBrand WHERE carBrand.brandName = :brandName", CarBrand.class);
+            query.setParameter("brandName", name);
+            return query.getSingleResult();
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+}
