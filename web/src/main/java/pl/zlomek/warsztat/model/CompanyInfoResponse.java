40bdf2f681dc1835a62177ea8eeb7c161a560366
Krzysztof Drzazga <drzazga_krzysztof@o2.pl>
2018-11-26 14:07:13 +0100

    quick edit

--- web/src/main/java/pl/zlomek/warsztat/model/CompanyInfoResponse.java
+++ web/src/main/java/pl/zlomek/warsztat/model/CompanyInfoResponse.java
@@ -1,8 +1,10 @@
 package pl.zlomek.warsztat.model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class CompanyInfoResponse extends CompanyForm {
     private long id;
 
